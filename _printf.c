eb9573aed0f362e52508579daabdb0a4be7ff632
yuslove1 <adexyuslove@gmail.com>
2023-04-02 13:55:54 -0700

    correction

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@ int _printf(const char * const format, ...)
 	convert_match m[] = {
 		{"%s", printf_string}, {"%c", printf_char},
 		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+		{"%i", printf_integer}, {"%d", printf_dec}, {"%r", printf_srev},
 		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
 		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
 		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
